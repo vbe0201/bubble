dc14b5db7ac213cf4b5a1a062ec17f139a02cea0
Valentin B. <valentin.be@protonmail.com>
2024-03-01 15:45:31 +0100

    Add syscall macro which handles variadic args

---
+++ source/common/macros/misc.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2024 Valentin B.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#define B__NARG__(...) B__NARG_I_(__VA_ARGS__, B__RSEQ_N())
+#define B__NARG_I_(...) B__ARG_N(__VA_ARGS__)
+#define B__ARG_N(                                     \
+     _1,  _2,  _3,  _4,  _5,  _6,  _7,  _8,  _9, _10, \
+    _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, \
+    _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, \
+    _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, \
+    _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, \
+    _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, \
+    _61, _62, _63, N, ...) N
+#define B__RSEQ_N()                         \
+    63, 62, 61, 60,                         \
+    59, 58, 57, 56, 55, 54, 53, 52, 51, 50, \
+    49, 48, 47, 46, 45, 44, 43, 42, 41, 40, \
+    39, 38, 37, 36, 35, 34, 33, 32, 31, 30, \
+    29, 28, 27, 26, 25, 24, 23, 22, 21, 20, \
+    19, 18, 17, 16, 15, 14, 13, 12, 11, 10, \
+     9,  8,  7,  6,  5,  4,  3,  2,  1,  0
+
+#define B__VMACRO_(name, n) name##n
+#define B__VMACRO(name, n) B__VMACRO_(name, n)
+#define B_VMACRO(func, ...) B__VMACRO(func, B__NARG__(__VA_ARGS__))
--- source/common/syscall.h
+++ source/common/syscall.h
@@ -17,9 +17,15 @@
 
 #include "types.h"
 #include "macros/arch.h"
+#include "macros/misc.h"
 
 #if defined(BUBBLE_ARCH_AMD64)
     #include "syscall/syscall.amd64.h"
 #else
     #error "Missing syscall implementation for target architecture!"
 #endif
+
+/**
+ * @brief Invokes a system call with a variable number of arguments.
+ */
+#define syscall(num, ...) B_VMACRO(syscall, __VA_ARGS__)(num, ##__VA_ARGS__)
